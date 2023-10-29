7300599b8f0fc812c5e39aa9764596a424225eab
AARAV SHETTY (897932) <897932@lcps.org>
2023-10-29 00:40:19 -0400

    Initial commit

---
+++ app/src/main/java/com/finalap/finalproject/MainActivity.java
@@ -0,0 +1,50 @@
+package com.finalap.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+                //Toast.makeText(getApplicationContext(), "hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
